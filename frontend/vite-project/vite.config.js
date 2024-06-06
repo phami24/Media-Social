e20f3072074d03e94962c2ac6ddca153b3e0638a
Minh Pham <buibinhminhpham2004@gmail.com>
2024-06-06 18:22:12 +0700

    vite config change

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -6,9 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    //Get rid of the CORS error 
     proxy: {
       "/api": {
         target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
